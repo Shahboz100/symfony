984201bcc411c7082c5db08f093f7c3445ccbba5
Shahboz100 <shahboz.100@yandex.ru>
2017-11-04 23:04:33 +0300

    add Pacient

---
+++ src/Blog/BlogBundle/Entity/Pacient.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: shahb
+ * Date: 04.11.2017
+ * Time: 21:49
+ */
+
+namespace Blog\BlogBundle\Entity;
+
+
+class Pacient
+{
+
+}
---
+++ src/Blog/BlogBundle/Entity/Privilege.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: shahb
+ * Date: 04.11.2017
+ * Time: 15:24
+ */
+
+namespace Blog\BlogBundle\Entity;
+
+
+class Privilege
+{
+
+}
